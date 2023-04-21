cc6d653768df1b395ce1850d62d31290409f1633
Wojtek <wojtek.karcz@gmail.com>
2023-04-21 23:13:46 +0200

    creating test function for data loading (csv)

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import streamlit as st
 
-def test_load(x):
-    return x*x
+csv = '../data/kc_house_data.csv'
+
+@st.cache_data
+def load_csv_data(csv):
+    df = pd.read_csv(csv, parse_dates=['date'], dayfirst=True)
+    return df
+
+
