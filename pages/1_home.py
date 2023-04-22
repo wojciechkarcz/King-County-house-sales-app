87b5cfe76328ee0a9a5cf3029eaafff851984aa7
Wojtek <wojtek.karcz@gmail.com>
2023-04-22 23:15:17 +0200

    file name changed

--- pages/1_home.py
+++ pages/1_home.py
@@ -14,17 +14,10 @@ st.write('House prices analysis app')
 st.markdown('test')
 
 
-def parameters():
-    sqft_living = st.slider('House area [sqft]:',290,13540,(290,13540))
-    sqft_lot = st.select_slider('Lot area [sqft]:',options=[520,5000,10000,15000,30000,1700000], value=(520,1700000))
-    return [sqft_living, sqft_lot]
+
 
 def main():
-    params = parameters()
-        
-    st.write(params[0])
-    st.write(params[1])
-
+   st.write('Placeholder for about page')
 
 
 if __name__ == '__main__':
