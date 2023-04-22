e4fd9a6426f27b58d7048f795ad4d15035544b0c
Wojtek <wojtek.karcz@gmail.com>
2023-04-22 22:58:56 +0200

    file moving and renaming

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import streamlit as st
+#import streamlit as st
 
-csv = '../data/kc_house_data.csv'
+#csv = '../data/kc_house_data.csv'
 
-@st.cache_data
+
 def load_csv_data(csv):
     df = pd.read_csv(csv, parse_dates=['date'], dayfirst=True)
     return df
