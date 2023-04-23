1a8c261eb3f7208fe4687a093ccb418eae745dfd
Wojtek <wojtek.karcz@gmail.com>
2023-04-23 17:41:05 +0200

    about page updated

--- pages/1_about.py
+++ pages/1_about.py
@@ -1,23 +1,61 @@
 import streamlit as st
-
-from src.data_loader import load_csv_data
-
-
-st.set_page_config(
-    page_title="King County House Sales",
-    page_icon="🏘️",
-    layout='centered'
-    )
-
-st.title('🏘️ Home')
-st.write('House prices analysis app')
-st.markdown('test')
+import pandas as pd
+from src.data_loader import load_data_sql
 
 
+@st.cache_data
+def data_load():
+	'''Function for data loading from PostgreSQL db and returning pandas dataframe'''
+	df = load_data_sql()
+	return df
+
+def get_metrics(df):
+    '''Getting some basic metrics from df''' 
+    df = df.sort_values(by='date')
+    total = len(df)
+    avg_price = round(df['price'].mean(),2)
+    avg_area = round(df['sqft_living'].mean(),2)
+    return total, avg_price, avg_area
 
 
 def main():
-   st.write('Placeholder for about page')
+   
+    st.set_page_config(
+    page_title="House sales in King County | About",
+    page_icon="🏘️",
+    layout='centered')
+    st.title('🏘️ About')
+
+    df = data_load()
+
+    st.image('img/kingcounty.jpg', caption='credit: Wikimedia', width=600)
+
+
+    st.markdown('This web application is used to analyze data on home sales in King County, USA. ' +
+                'The data has been made available for public use and can be downloaded from the website:  ' +
+                 '[https://www.kaggle.com/datasets/harlfoxem/housesalesprediction](https://www.kaggle.com/datasets/harlfoxem/housesalesprediction)')
+
+    st.markdown('##')
+
+    st.markdown('Some basic metrics of the dataset')
+
+    total, avg_price, avg_area = get_metrics(df)
+
+    col1, col2, col3  = st.columns(3)
+
+    with col1:
+         st.metric(('Number of transactions'), total)
+    with col2:
+         st.metric(('Avg. price $'), avg_price)
+    with col3:
+         st.metric(('Avg. area [sqft]'), avg_area)
+    
+    st.markdown('###')
+
+    st.markdown('''
+    Detailed documentation describing the data set can be found at this link:  
+    [https://geodacenter.github.io/data-and-lab/KingCounty-HouseSales2015/](https://geodacenter.github.io/data-and-lab/KingCounty-HouseSales2015/)
+    ''')
 
 
 if __name__ == '__main__':
